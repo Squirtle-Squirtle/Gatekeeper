989413c56a2c2d5875119979cd42d3b55abda9a0
Squirtle-Squirtle <ak.abhishekkr24@gmail.com>
2025-08-17 14:57:58 +0530

    Initial Repo Create with Spring

---
+++ src/test/java/com/slay/ratelimitter/GatekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slay.ratelimitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
