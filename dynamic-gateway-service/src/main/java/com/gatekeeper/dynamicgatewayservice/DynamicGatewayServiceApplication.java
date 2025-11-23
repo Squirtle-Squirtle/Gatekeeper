622a29236dc01699d18cb272822a770f87c99eec
Squirtle-Squirtle <ak.abhishekkr24@gmail.com>
2025-11-23 22:24:38 +0530

    Resolved errors for dynamicgatewayservice Modified pom and java version for models and gateway service

--- dynamic-gateway-service/src/main/java/com/gatekeeper/dynamicgatewayservice/DynamicGatewayServiceApplication.java
+++ dynamic-gateway-service/src/main/java/com/gatekeeper/dynamicgatewayservice/DynamicGatewayServiceApplication.java
@@ -2,7 +2,7 @@ package com.gatekeeper.dynamicgatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.gatekeeper.models")
