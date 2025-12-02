f41684dcd2f2f41694990feecc3f2407f8f24a0f
Squirtle-Squirtle <ak.abhishekkr24@gmail.com>
2025-12-02 23:56:17 +0530

    Added Gateway config and build success confirmed

--- dynamic-gateway-service/src/main/java/com/gatekeeper/dynamicgatewayservice/GatewayConfig.java
+++ dynamic-gateway-service/src/main/java/com/gatekeeper/dynamicgatewayservice/GatewayConfig.java
@@ -1,35 +1,35 @@
-//package com.gatekeeper.dynamicgatewayservice;
-//
-//import com.gatekeeper.models.Route;
-//import org.springframework.cloud.gateway.route.*;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.context.annotation.*;
-//
-//import java.net.URI;
-//import java.util.List;
-//
-//@Configuration
-//public class GatewayConfig {
-//
-//    @Bean
-//    public RouteLocator dynamicRoutes(RouteLocatorBuilder builder,
-//                                      DynamicRouteService dynamicRoutesService,
-//                                      RateLimiterConfig rateLimiterConfig) {
-//
-//        List<Route> routes = dynamicRoutesService.loadRoutes();
-//
-//        RouteLocatorBuilder.Builder routesBuilder = builder.routes();
-//
-//        routes.forEach(r -> routesBuilder.route(
-//                r.getId().toString(),
-//                p -> p.path(r.getPath())
-//                        .filters(f -> f.requestRateLimiter(c -> {
-//                            c.setKeyResolver(rateLimiterConfig.userKeyResolver());
-//                            c.setRateLimiter(rateLimiterConfig.redisRateLimiter());
-//                        }))
-//                        .uri(URI.create(r.getDestinationUri()))
-//        ));
-//
-//        return routesBuilder.build();
-//    }
-//}
+package com.gatekeeper.dynamicgatewayservice;
+
+import com.gatekeeper.models.Route;
+import org.springframework.cloud.gateway.route.*;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator dynamicRoutes(RouteLocatorBuilder builder,
+                                      DynamicRouteService dynamicRoutesService,
+                                      RateLimiterConfig rateLimiterConfig) {
+
+        List<Route> routes = dynamicRoutesService.loadRoutes();
+
+        RouteLocatorBuilder.Builder routesBuilder = builder.routes();
+
+        routes.forEach(r -> routesBuilder.route(
+                r.getId().toString(),
+                p -> p.path(r.getPath())
+                        .filters(f -> f.requestRateLimiter(c -> {
+                            c.setKeyResolver(rateLimiterConfig.userKeyResolver());
+                            c.setRateLimiter(rateLimiterConfig.redisRateLimiter());
+                        }))
+                        .uri(URI.create(r.getDestinationUri()))
+        ));
+
+        return routesBuilder.build();
+    }
+}
